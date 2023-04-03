a44ee8a9ecdb267f3ffe5e6dd0a58e80bb3db648
hiareum <j_ar2979@naver.com>
2023-04-03 11:29:15 +0900

    230403 spring책 class05 어노테이션기반 AOP

---
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -0,0 +1,28 @@
+package com.springbook.biz.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
+
+@Service
+@Aspect
+public class AroundAdvice {
+
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {
+	}
+	
+	@Around("allPointcut()") //하나의 어드바이스로 사전, 사후 처리를 모두 해결하고자 할 때 사용 ,Around메소드에서만 joinPoint가 아닌 proceeding
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+		String method = pjp.getSignature().getName();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Object obj = pjp.proceed();
+		stopWatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+		return obj;
+	}
+}
