54d12eb4b2aff26b9a1d99c801f4b71a967b6248
hiareum <j_ar2979@naver.com>
2023-04-04 11:49:00 +0900

    230404 스프링교재 class07 262p 까지

--- src/main/java/com/springbook/biz/board/BoardServiceCilent.java
+++ src/main/java/com/springbook/biz/board/BoardServiceCilent.java
@@ -9,13 +9,17 @@ public class BoardServiceCilent {
 	public static void main(String[] args) {
 //1.스프링컨테이너 구동
 		AbstractApplicationContext container=new GenericXmlApplicationContext("applicationContext.xml");
-	//2.스프링 컨테이너로부터 boardserviceImpl 객체(boardservice)를 LookUp함
+	
+		//2.스프링 컨테이너로부터 boardserviceImpl 객체(boardservice)를 LookUp함
 		BoardService boardService=(BoardService) container.getBean("boardService");
+		
 		//3.글 등록 기능 테스트
 		BoardVO vo=new BoardVO();
+	
 		vo.setTitle("임시제목");
 		vo.setWriter("홍길동");
 		vo.setContent("임시내용...");
+		
 		boardService.insertBoard(vo);
 		
 		//4.글 목록 검색 기능 테스트
--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springbook.biz.board.BoardDAOSpring;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.common.Log4jAdvice;
@@ -13,7 +14,7 @@ import com.springbook.biz.common.LogAdvice;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
 	//private LogAdvice log;
 //	private Log4jAdvice log;
 
@@ -26,6 +27,7 @@ public class BoardServiceImpl implements BoardService {
 	//	log.printLog();
 		//log.printLogging();
 		boardDAO.insertBoard(vo);
+		//boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
