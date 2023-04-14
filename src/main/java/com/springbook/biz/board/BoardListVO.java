ac88f0683aa6ff29eacac76b24f1c0917874cb21
hiareum <j_ar2979@naver.com>
2023-04-14 12:04:36 +0900

    230414 스프링교재456pg까지 다국어기능

---
+++ src/main/java/com/springbook/biz/board/BoardListVO.java
@@ -0,0 +1,27 @@
+package com.springbook.biz.board;
+
+
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+//Xml문서는 반드시 단 하나의 루트 엘리먼트를 가져야 한다. 여러 게시글 목록을 xml로 표현해야 하므로 boardVO객체 여러개를
+//루트 엘리먼트로 사용할 또 다른 자바 클래스가 필요
+@XmlRootElement(name = "boardList") //루트 엘리먼트로 설정
+@XmlAccessorType(XmlAccessType.FIELD)
+public class BoardListVO {
+	@XmlElement(name = "board") //이 설정이 없으면 boardList가 엘리먼트 이름으로 사용 따라서 board로 변경
+	private List<BoardVO> boardList;
+
+	public List<BoardVO> getBoardList() {
+		return boardList;
+	}
+
+	public void setBoardList(List<BoardVO> boardList) {
+		this.boardList = boardList;
+	}
+}
--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -5,6 +5,7 @@ package com.springbook.view.board;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.springbook.biz.board.BoardListVO;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
@@ -123,4 +126,15 @@ public class BoardController {
 		}
 		
 		
-}
+		@RequestMapping("/dataTransform.do")
+		@ResponseBody //자바 객체를 Http응답 프로토콜의 몸체로 변환하기 위해 사용
+		public BoardListVO dataTransform(BoardVO vo) {
+			vo.setSearchCondition("TITLE");
+			vo.setSearchKeyword("");
+			List<BoardVO> boardList = boardService.getBoardList(vo);
+			BoardListVO boardListVO = new BoardListVO();
+			boardListVO.setBoardList(boardList);
+			
+			return boardListVO;
+		
+}}
